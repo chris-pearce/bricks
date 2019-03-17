c873c60352ac4656179b2dbdcce5e89b66839f5a
Chris Pearce <crite70@gmail.com>
2019-03-17 10:19:13 +0100

    consume tokens in spacing package

--- packages/spacing/src/Spacing.js
+++ packages/spacing/src/Spacing.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import propTypes from 'prop-types';
+import { TokensConsumer } from '@bricks/tokens-provider';
+import { getKeyFromValue } from '@bricks/utils';
 
 const Spacing = ({ children, isUpwards, size, ...props }) => (
-  <div {...props} style={{ [isUpwards ? 'marginTop' : 'marginBottom']: size }}>
-    {children}
-  </div>
+  <TokensConsumer>
+    {tokens => (
+      <div
+        {...props}
+        className={tokens && `bricks-spacing-${getKeyFromValue(size, tokens)}`}
+        style={
+          !tokens ? { [isUpwards ? 'marginTop' : 'marginBottom']: size } : null
+        }
+      >
+        {children}
+      </div>
+    )}
+  </TokensConsumer>
 );
 
 Spacing.propTypes = {
